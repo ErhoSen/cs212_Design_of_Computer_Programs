1d246ab1577b6f879fa45cff2aa4168f07a50c12
ErhoSen <erhosen@gmail.com>
2015-02-23 22:44:50 +0300

    Lesson 4 part 1

---
+++ Lesson4/BridgeSucc.py
@@ -0,0 +1,44 @@
+# -----------------
+# User Instructions
+# 
+# Write a function, bsuccessors(state), that takes a state as input
+# and returns a dictionary of {state:action} pairs.
+#
+# A state is a (here, there, t) tuple, where here and there are 
+# frozensets of people (indicated by their times), and potentially
+# the 'light,' t is a number indicating the elapsed time.
+#
+# An action is a tuple (person1, person2, arrow), where arrow is 
+# '->' for here to there or '<-' for there to here. When only one 
+# person crosses, person2 will be the same as person one, so the
+# action (2, 2, '->') means that the person with a travel time of
+# 2 crossed from here to there alone.
+import itertools
+
+
+def bsuccessors(state):
+    """Return a dict of {state:action} pairs. A state is a (here, there, t) tuple,
+    where here and there are frozensets of people (indicated by their times) and/or
+    the 'light', and t is a number indicating the elapsed time. Action is represented
+    as a tuple (person1, person2, arrow), where arrow is '->' for here to there and
+    '<-' for there to here."""
+    here, there, t = state
+    heretothere = 'light' in here
+    dir = '->' if heretothere else '<-'
+    return {(here ^ frozenset([p1, p2, 'light']),
+             there ^ frozenset([p1, p2, 'light']),
+             t + max(int(p1), int(p2))) : (p1, p2, dir)
+            for p1 in (here if heretothere else there) if p1 != 'light'
+            for p2 in (here if heretothere else there) if p2 != 'light'}
+
+
+def test():
+    assert bsuccessors((frozenset([1, 'light']), frozenset([]), 3)) == {
+                (frozenset([]), frozenset([1, 'light']), 4): (1, 1, '->')}
+
+    assert bsuccessors((frozenset([]), frozenset([2, 'light']), 0)) =={
+                (frozenset([2, 'light']), frozenset([]), 2): (2, 2, '<-')}
+
+    return 'tests pass'
+
+print test()
